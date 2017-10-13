22665ae0b61efeae0ef664e358423a701602e37c
Petar Jager <petar.jager@gmail.com>
2017-10-13 14:41:41 +0200

    work on balancer.... ongoing

--- src/goshovel/goshovel.go
+++ src/goshovel/goshovel.go
@@ -5,6 +5,7 @@ import (
 	"net"
     "io"
     "os"
+    "sort"
     "goshovel/config"
     "gopkg.in/yaml.v2"
 )
@@ -24,6 +25,7 @@ func shovel(srcconn, dstconn net.Conn) {
     }
 }
 
+// handler for incoming connections
 func handleConnection(lconn net.Conn) {
 	defer lconn.Close()
 	fmt.Println("Got connection from ", lconn.RemoteAddr())
@@ -36,15 +38,59 @@ func handleConnection(lconn net.Conn) {
         panic("ERROR: net.Dial()")
     }
 
+    // do work l->r
     go shovel(lconn, rconn)
+    // do work r->l
     shovel(rconn, lconn)
 
     fmt.Println("EXIT: handleConnection()")
 }
 
+
+// set up data struct to unmarshall from config file
+type Server struct {
+    Ip string
+    Port string
+    Enabled bool
+    connCount int
+}
+
+type Config struct {
+    GoShovel Server `yaml:"GoShovel"`
+    Backends map[string]Server `yaml:"Backends"`
+}
+
+func (c *Config) dump_backends() {
+    fmt.Println("Goshovel server (from config):")
+    fmt.Println("> ", c.GoShovel.Ip, c.GoShovel.Port, c.GoShovel.Enabled ,c.GoShovel.connCount)
+    fmt.Println("Backend servers (from config):")
+    for k,v:=range c.Backends {
+        fmt.Println("> ",k, " - ", v)
+    }
+}
+
+func (c *Config) get_next() {
+    fmt.Println("Len: ", len(c.Backends))
+    s:=make([]int, 0, len(c.Backends))
+    for _,v:=range c.Backends {
+        if v.Enabled==true {
+            s=append(s, v.connCount)
+        }
+    }
+    sort.Ints(s)
+
+    for _,v:=range c.Backends {
+        if v.Enabled==true && v.connCount==s[0] {
+            break
+        }
+    }
+    // open connection to backend (R-side) server
+
+    fmt.Println("Sorted: ", s)
+}
+
 func main() {
     config.Hello()
-
 
     // check env variable for the config file path
     val, present := os.LookupEnv(ENV_CONFIG)
@@ -62,31 +108,23 @@ func main() {
         panic("ERROR: os.Open()")
     }
 
-    // set up data struct to unmarshall from config file
-    type Serv struct {
-        Ip string
-        Port string
-    }
-
-    var srv map[string]Serv
-
     // read & unmarshal config
     fi, _:=confFile.Stat()
     config:=make([]byte, fi.Size())
     confFile.Read(config)
-    fmt.Println(config)
+    // fmt.Println(config)
 
-    yaml.Unmarshal(config, &srv)
+    var conf Config
+    yaml.Unmarshal(config, &conf)
 
-    for k,v:=range srv {
-        fmt.Println(k," - ",v)
-    }
+    conf.dump_backends()
+    conf.get_next()
 
     // starting with the networking stuff
     fmt.Println("Starting listener...")
 	//listen, err := net.Listen("tcp4", ":9999")
-	fmt.Println("Listening on: ",srv["goshovel"].Ip+":"+srv["goshovel"].Port)
-	listen, err := net.Listen("tcp4", srv["goshovel"].Ip+":"+srv["goshovel"].Port)
+	fmt.Println("Listening on: ",conf.GoShovel.Ip+":"+conf.GoShovel.Port)
+	listen, err := net.Listen("tcp4", conf.GoShovel.Ip+":"+conf.GoShovel.Port)
 	if err != nil {
         fmt.Println(err)
 		panic("ERROR: listen()")
